6bbf52039bfc829b1dbbd761dd1465b6b7cb3028
Yoongon-Kim <yooongonkim@gmail.com>
2025-12-29 03:14:25 +0000

    test

--- utils/setting.py
+++ utils/setting.py
@@ -1,7 +1,7 @@
 import os
-cache_dir = None #'/scratch/pbsjobs/wxy320/huggingface'
-project_dir = '/workspace/speculative_thinking' #'/home/wxy320/ondemand/program/speculative_thinking'
-hug_token = None #'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'
+cache_dir = None
+project_dir = '/workspace/speculative_thinking'
+hug_token = None
 if cache_dir is not None:
     os.environ["HF_HOME"] = cache_dir
 
@@ -10,6 +10,3 @@ if hug_token is not None:
     login(token=hug_token)
 
 api_keys = None
-"""{
-    'openai': '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'
-}"""
